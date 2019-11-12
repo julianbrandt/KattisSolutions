d1289b85d128d0af7119ea50d51d3375d2a2b6db
julianbrandt <julianbrandt@godmail.dk>
2019-11-12 12:06:37 +0100

    symmetric order

--- KattisSolutions/SymmetricOrder/Program.cs
+++ KattisSolutions/SymmetricOrder/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace SymmetricOrder
 {
@@ -12,9 +14,32 @@ namespace SymmetricOrder
             {
                 set++;
                 Console.WriteLine("SET " + set);
+                var names = new List<string>();
                 for (var i = 0; i < lines; i++)
                 {
-                    Console.WriteLine(Console.ReadLine());
+                    names.Add(Console.ReadLine());
+                }
+
+                names.Sort((a, b) => a.Length.CompareTo(b.Length));
+                var sortedNames = new List<string>();
+                var counter = 0;
+                while (counter < names.Count)
+                {
+                    sortedNames.Add(names[counter]);
+                    counter += 2;
+                }
+                
+                if (names.Count % 2 == 0) counter = names.Count - 1;
+                else counter = names.Count - 2;
+                while (counter > 0)
+                {
+                    sortedNames.Add(names[counter]);
+                    counter -= 2;
+                }
+
+                foreach (var name in sortedNames)
+                {
+                    Console.WriteLine(name);
                 }
             }
         }
