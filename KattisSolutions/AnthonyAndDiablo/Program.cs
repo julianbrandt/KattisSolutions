4b293e8d4ee70cde2e498ac5dc10c01f2246e170
julianbrandt <julianbrandt@godmail.dk>
2019-11-12 22:59:53 +0100

    anthony and diablo

---
+++ KattisSolutions/AnthonyAndDiablo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace AnthonyAndDiablo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(" ").Select(double.Parse).ToList();
+            var size = input[0];
+            var fence = input[1];
+            
+            if (areaOfCircle(fence) > size) Console.WriteLine("Diablo is happy!");
+            else Console.WriteLine("Need more materials!");
+        }
+        
+        static double areaOfCircle(double fence)
+        {
+            var radius = fence / Math.PI / 2;
+            return Math.PI * Math.Pow(radius, 2);
+        }
+    }
+}
