a5a2b0b58d1d7e524d8416f92ea241d0e51edd22
julianbrandt <julianbrandt@godmail.dk>
2019-11-23 23:08:25 +0100

    romans

---
+++ KattisSolutions/Romans/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Romans
+{
+    class Program
+    {
+        private static double paces(double d)
+        {
+            const double ratio = (5280.0 / 4854.0) * 1000.0;
+            return Math.Round(d * ratio);
+        }
+        
+        static void Main(string[] args)
+        {
+            var miles = double.Parse(Console.ReadLine());
+             Console.WriteLine(paces(miles));
+        }
+    }
+}
